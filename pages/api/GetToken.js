8943753705478b680b00d15e442301c48cec251e
Adan Einstein <adaneinstein@gmail.com>
2022-06-05 13:53:58 -0300

    gettoken

--- pages/api/GetToken.js
+++ pages/api/GetToken.js
@@ -1,7 +1,8 @@
+import axios from "axios";
 export default async function GetToken(request, response) {
     const login = request.query.login;
     const password = request.query.password;
-    const api = await fetch("https://agendaqui-api.herokuapp.com/login");
-    api.body = JSON.stringify({ login, password });
+    const body = JSON.stringify({ login, password });
+    const api = await axios.get("");
     response(api.status(200).json());
 }
