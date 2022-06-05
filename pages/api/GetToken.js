5b12eba4da598c4d519927dbae3348109e4bf022
Adan Einstein <adaneinstein@gmail.com>
2022-06-05 13:59:53 -0300

    gettoken axios 2

--- pages/api/GetToken.js
+++ pages/api/GetToken.js
@@ -2,10 +2,12 @@ import axios from "axios";
 export default async function GetToken(request, response) {
     const login = request.query.login;
     const password = request.query.password;
-    const body = JSON.stringify({ login, password });
     const api = await axios.post(
         "https://sistema-agendaqui.vercel.app/api/gettoken",
-        body,
+        {
+            login,
+            password,
+        },
         { "Content-Type": "application/json" }
     );
     response(api.status(200).json());
