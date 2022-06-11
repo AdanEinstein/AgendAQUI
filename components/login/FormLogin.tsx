63075b267ab8d38c1ee20a71001cc67585136d2b
AdanEinstein <adaneinstein@gmail.com>
2022-06-11 15:06:13 -0300

    Cadastro de cliente e prestador (apenas form validate) 2

--- components/login/FormLogin.tsx
+++ components/login/FormLogin.tsx
@@ -10,7 +10,7 @@ import * as yup from "yup";
 yup.setLocale({
 	mixed: {
 		required(params) {
-			return `${params.path} não foi preenchido!`
+			return `${params.path} não foi preenchido!`;
 		},
 	},
 });
@@ -20,12 +20,14 @@ const schema = yup.object().shape({
 	login: yup.string().required().label("Login"),
 });
 
+const feedbackDefault = {
+	icon: "bi bi-info-circle",
+	message: "Digite corretamente as informações",
+	color: "text-primary",
+};
+
 const FormLogin: React.FC = () => {
-	const [feedback, setFeedback] = useState<IFeedback>({
-		icon: "bi bi-info-circle",
-		message: "Digite corretamente as informações",
-		color: "text-primary",
-	});
+	const [feedback, setFeedback] = useState<IFeedback>(feedbackDefault);
 	const [loading, setLoading] = useState<boolean>(false);
 	const loginRef = useRef<HTMLInputElement>();
 	const passwordRef = useRef<HTMLInputElement>();
@@ -36,42 +38,49 @@ const FormLogin: React.FC = () => {
 			login: loginRef.current.value,
 			password: passwordRef.current.value,
 		};
-		schema.validate(user).then(() => {
-			axios.post(`${profileEnv.baseUrl}/gettoken`, user)
-				.then((token) => {
-					if (token.status === 200) {
-						localStorage.setItem("token", token.data);
-						setFeedback({
-							icon: "bi bi-check-circle",
-							message: "Login válido",
-							color: "text-success",
-						});
-						setLoading(false);
-					} else {
+		schema
+			.validate(user)
+			.then(() => {
+				setFeedback(feedbackDefault);
+			})
+			.then(() => {
+				axios
+					.post(`${profileEnv.baseUrl}/gettoken`, user)
+					.then((token) => {
+						if (token.status === 200) {
+							localStorage.setItem("token", token.data);
+							setFeedback({
+								icon: "bi bi-check-circle",
+								message: "Login válido",
+								color: "text-success",
+							});
+							setLoading(false);
+						} else {
+							setFeedback({
+								icon: "bi bi-exclamation-diamond-fill",
+								message: token.data,
+								color: "text-warning",
+							});
+							setLoading(false);
+						}
+					})
+					.catch((err: AxiosError) => {
 						setFeedback({
 							icon: "bi bi-exclamation-diamond-fill",
-							message: token.data,
+							message: "Informações inválidas!",
 							color: "text-warning",
 						});
 						setLoading(false);
-					}
-				})
-				.catch((err: AxiosError) => {
-					setFeedback({
-						icon: "bi bi-exclamation-diamond-fill",
-						message: "Informações inválidas!",
-						color: "text-warning",
 					});
-					setLoading(false);
+			})
+			.catch((err: yup.ValidationError) => {
+				setFeedback({
+					icon: "bi bi-exclamation-triangle",
+					message: err.errors,
+					color: "text-danger",
 				});
-		}).catch((err: yup.ValidationError) => {
-			setFeedback({
-				icon: "bi bi-exclamation-triangle",
-				message: err.errors,
-				color: "text-danger",
+				setLoading(false);
 			});
-			setLoading(false);
-		});
 	}, [loginRef, passwordRef]);
 
 	useEffect(() => {
