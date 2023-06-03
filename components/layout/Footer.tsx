9f3291905a987dc6e71f4c65b2da112b4d7f3042
AdanEinstein <adaneinstein@gmail.com>
2023-06-03 10:04:22 -0300

    Redeploy

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link"
 import styles from "/styles/Footer.module.css"
 const Footer: React.FC = () => {
     return (
         <footer className={styles.Footer}>
-            <p className="m-0">&copy; Todos os direitos reservados</p>
+            <p className="m-0">&copy; Todos os direitos reservados -
+                <Link href={'https://www.linkedin.com/in/adaneinstein/'}>
+                    <span className="link-primary" style={{cursor: "pointer"}}>{' '}Adan Einstein <i className="bi bi-linkedin"></i></span>
+                </Link>
+            </p>
         </footer>
     )
 }
